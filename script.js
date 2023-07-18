8cb1c06d787155b6c9e4ea44a17f038c4358af35
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-07-18 13:07:35 +0100

    learning material 3

--- script.js
+++ script.js
@@ -71,7 +71,9 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       index + 1
     } ${type}</div>
-      <div class="movements__value">${movement}€</div>
+      <div class="movements__value">${
+        type === "deposit" ? "£" + movement : "-£" + Math.abs(movement)
+      }</div>
     </div>
     `;
 
@@ -88,6 +90,28 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `£${balance}`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `£${incomes}`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `£${Math.abs(out)}`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, interest) => acc + interest, 0);
+  labelSumInterest.textContent = `£${interest}`;
+};
+calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
